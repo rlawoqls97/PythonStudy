656b3255582b58141e5446a093925ca07d3918dc
jaejae-kim <21600134@handong.edu>
2021-09-16 18:57:25 +0900

    Python 5th day

--- first.py
+++ first.py
@@ -1,5 +1,6 @@
 # Study python because of Coding test
 # Algorithm with python
+# 코딩테스트를 파이썬으로 치루기 위한 공부
 
 
 
@@ -276,3 +277,33 @@
 # d = [6, 8, 3, 9, 10, 1, 2, 4, 7, 5]
 # merge_sort_re(d)
 # print(d)
+
+# kor = float(input("국어성적 입력: "))
+# eng = float(input("영어성적 입력: "))
+# math = float(input("수학성적 입력: "))
+# avg = (kor + eng + math) / 3
+
+# if avg > 60 and kor >= 50 and eng >= 50 and math >= 50:
+#     print("성적평균은", avg, "이미, 과락 과목도 없기 때문에 합격입니다")
+# else:
+#     if avg >= 60:
+#         print("성적평균은", avg, "이지만 50점 미만 과목이 있어서 불합격 입니다.")
+#     else:
+#         print("성적평균은", avg, "이며 불합격입니다.")
+
+# height = float(input("키를 m 단위로 입력해 주세요: "))
+# weight = float(input("몸무게를 kg단위로 입력해 주세요: "))
+# bmi = weight / (height * height)
+
+# if bmi < 18.5:
+#     print("BMI지수는", bmi, "이며 저체중 상태입니다.")
+# elif bmi < 23:
+#     print("BMI지수는", bmi, "이며 정상 상태입니다.")
+# elif bmi < 25:
+#     print("BMI지수는", bmi, "이며 과체중 상태입니다.")
+# elif bmi < 30:
+#     print("BMI지수는", bmi, "이며 비만1 상태입니다.")
+# elif bmi < 40:
+#     print("BMI지수는", bmi, "이며 비만2 상태입니다.")
+# else:
+#     print("BMI지수는", bmi, "이며 심각한 비만3 상태입니다.")
